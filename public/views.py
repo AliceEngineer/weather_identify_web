53bcaec762f615275e270fabb807c224d16e7b35
Tinger <devil.home@foxmail.com>
2022-10-09 21:54:38 +0800

    add app public

---
+++ public/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+
+class PublicTesting(View):
+    
+    def get(self, request):
+        
+        return redirect(to=reverse('weather:converter_images_to_base64'))
